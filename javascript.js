47d2c29cff94e7ed7d56a52d1fb222bbe5b6f56b
Aayush <coolaayushrai@gmail.com>
2022-09-07 16:15:31 +0530

    Add first function to JS
    Add variables for player and
    computer selection.
    Add function for computer choice.
    Use random int value. Get result using
    conditionals.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,21 @@
 let userName = prompt("Hello, what's your name?");
 alert(`Welcome, ${userName}!\nGet ready to face off against The Computer!\nYou may enter 'r', 'p', or 's' and the game will still work.`);
+let playerSelection = "RPS";
+let computerSelection = 10;
+
+function getComputerChoice()
+{
+	let randomAssigner = Math.floor(Math.random() * 3);
+	if (randomAssigner === 0)
+	{
+		computerSelection = 1;
+	}
+	else if (randomAssigner === 1)
+	{
+		computerSelection = 2;
+	}
+	else 
+	{
+		computerSelection = 3;
+	}
+}
