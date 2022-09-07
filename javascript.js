d7fbf7bb247ca7bf966b027f07ab7b376470bae1
Aayush <coolaayushrai@gmail.com>
2022-09-07 16:12:47 +0530

    Minor additions to html and js
    Add css and list to html. Add greeting
    in JS file.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,2 @@
-console.log("Hello World!");
+let userName = prompt("Hello, what's your name?");
+alert(`Welcome, ${userName}!\nGet ready to face off against The Computer!\nYou may enter 'r', 'p', or 's' and the game will still work.`);
