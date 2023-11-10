ee314105d7bb71fc4fdaa906c96c1909bc3f47f4
Aayush Rai <coolaayushrai@gmail.com>
2023-11-10 17:49:19 +0530

    Updated game() function.
    Initialize counter for player
    and computer wins. Show final score
    and final result at the end of 5 matches.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+let playerWins = 0;
+let computerWins = 0;
+
 game();
 
 function getComputerChoice()
 {
     let randomNum = 0;
     let outcome = "";
-    
+
     while (randomNum == 0 || randomNum > 3)
     {
         randomNum = Math.floor(Math.random() * 10);
@@ -55,26 +58,32 @@ function playRound(playerSelection, computerSelection)
     else if ((playerSelection === 'rock') && (computerSelection === 'scissors'))
     {
         alert("You WIN! Rock breaks Scissors!");
+        playerWins++;
     }
     else if ((playerSelection === 'paper') && (computerSelection === 'rock'))
     {
         alert("You WIN! Paper wraps Rock!");
+        playerWins++;
     }
     else if ((playerSelection === 'scissors') && (computerSelection === 'paper'))
     {
         alert("You WIN! Scissors cut Paper!");
+        playerWins++;
     }
     else if ((playerSelection === 'rock') && (computerSelection === 'paper'))
     {
         alert("You LOSE! Rock gets wrapped by Paper!");
+        computerWins++;
     }
     else if ((playerSelection === 'paper') && (computerSelection === 'scissors'))
     {
         alert("You LOSE! Paper gets cut by Scissors!")
+        computerWins++;
     }
     else
     {
         alert("You LOSE! Scissors gets smashed by Rock!");
+        computerWins++;
     }
 }
 
@@ -83,5 +92,30 @@ function game()
     for (let times=0; times < 5; times++)
     {
         playRound(getPlayerChoice(), getComputerChoice());
+    }
+
+    if (playerWins > computerWins)
+    {
+        alert(
+            "You: " + playerWins +
+            "\nComputer: " + computerWins +
+            "\n\nCongratulations! You're the WINNER!"
+        )
+    }
+    else if(playerWins < computerWins)
+    {
+        alert(
+            "You: " + playerWins +
+            "\nComputer: " + computerWins +
+            "\n\nComputer is the WINNER!"
+        )
+    }
+    else
+    {
+        alert(
+            "You: " + playerWins +
+            "\nComputer: " + computerWins +
+            "\n\nDRAW!"
+        )
     }
 }
