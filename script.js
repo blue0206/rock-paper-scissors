cce2469ec211a4ff010b0097d70e7ad368d5d102
Aayush Rai <coolaayushrai@gmail.com>
2023-11-10 12:47:37 +0530

    Create function to play single round of game.

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ function getComputerChoice()
 
     if (randomNum == 1)
     {
-        return outcome = "Rock";
+        return outcome = "rock";
     }
     else if (randomNum == 2)
     {
-        return outcome = "Paper";
+        return outcome = "paper";
     }
     else
     {
-        return outcome = "Scissors";
+        return outcome = "scissors";
     }
 }
 
@@ -30,12 +30,49 @@ function getPlayerChoice()
     {
         response = prompt("Enter 'Rock', 'Paper', or 'Scissors'!");
         response = response.toLowerCase();
-        
+
         if (response === 'rock' || response === 'paper' || response == 'scissors' || response === 'scissor')
         {
             break;
         }
     }
 
+    if (response === 'scissor')
+    {
+        response = 'scissors';
+    }
+
     return response;
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    if (playerSelection === computerSelection)
+    {
+        alert("It's a TIE!");
+    }
+    else if ((playerSelection === 'rock') && (computerSelection === 'scissors'))
+    {
+        alert("You WIN! Rock breaks Scissors!");
+    }
+    else if ((playerSelection === 'paper') && (computerSelection === 'rock'))
+    {
+        alert("You WIN! Paper wraps Rock!");
+    }
+    else if ((playerSelection === 'scissors') && (computerSelection === 'paper'))
+    {
+        alert("You WIN! Scissors cut Paper!");
+    }
+    else if ((playerSelection === 'rock') && (computerSelection === 'paper'))
+    {
+        alert("You LOSE! Rock gets wrapped by Paper!");
+    }
+    else if ((playerSelection === 'paper') && (computerSelection === 'scissors'))
+    {
+        alert("You LOSE! Paper gets cut by Scissors!")
+    }
+    else
+    {
+        alert("You LOSE! Scissors gets smashed by Rock!");
+    }
 }
